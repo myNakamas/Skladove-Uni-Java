80ad897e8267b95d25e754fa9ffcbb4710e28090
Angel <angel.g.prodanov@gmail.com>
2021-01-25 15:52:07 +0200

    setup 1.3 - hibernate

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module Skladove {
     requires javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.hibernate.orm.core;
     opens App.Controllers to javafx.fxml;
 
     opens App;
