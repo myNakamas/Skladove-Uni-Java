4fd235c07b5d29f7c79f1d8511292b4b62114419
marti <martinavalkova1999@gmail.com>
2021-01-30 11:41:51 +0200

    prettier

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import util.HibernateUtil;
@@ -15,10 +16,10 @@ public class App extends Application {
             HibernateUtil.getSessionFactory().openSession();
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("scenes/LogIn.fxml")));
             Scene s = new Scene(root);
-            primaryStage.setTitle("test");
+            primaryStage.setTitle("Storage");
             primaryStage.setScene(s);
             primaryStage.show();
-
+            primaryStage.getIcons().add(new Image("images/house.png"));
             Platform.setImplicitExit(true);
             primaryStage.setOnCloseRequest((ae) -> {
                 Platform.exit();
