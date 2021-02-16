06810ffcef9a71b33f8c8d4bb2ef15d9cd242e0d
usagichi <geri_ivanova_@abv.bg>
2021-02-16 17:13:54 +0200

    Agent stuff to be done

--- src/main/java/Controllers/LogInController.java
+++ src/main/java/Controllers/LogInController.java
@@ -2,19 +2,31 @@ package Controllers;
 
 import Services.UserService;
 import entities.User;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import util.HibernateUtil;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class LogInController implements Initializable {
 
     public TextField username;
     public PasswordField password;
+
+    Button btn1 = new Button();
+    Button btn2 = new Button();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -39,7 +51,7 @@ public class LogInController implements Initializable {
 
     }
 
-    public void showAccountData() {
+   /* public void showAccountData() {
         UserService userService = new UserService();
         User newUser = new User();
         String uName = username.getText();
@@ -50,5 +62,22 @@ public class LogInController implements Initializable {
 
         userService.persist(newUser);
 
+    }*/
+
+    public void showRegisterScene(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("scenes/AgentScene.fxml")));
+        HibernateUtil.getSessionFactory().openSession();
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("scenes/LogIn.fxml")));
+        Scene s = new Scene(root);
+        primaryStage.setTitle("Storage");
+        primaryStage.setScene(s);
+        primaryStage.show();
+        primaryStage.getIcons().add(new Image("images/house.png"));
+        Platform.setImplicitExit(true);
+        primaryStage.setOnCloseRequest((ae) -> {
+            Platform.exit();
+            System.exit(0);
+        }
     }
+
 }
