5ea8452cb77bbd1ab94ba86876b29feab70ff3a4
Angel <angel.g.prodanov@gmail.com>
2021-03-04 15:03:50 +0200

    OwnerScene 1.0

---
+++ src/main/java/Controllers/OwnerController.java
@@ -0,0 +1,26 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+
+public class OwnerController {
+    public TableColumn colName;
+    public TableColumn colDimentions;
+    public TableColumn colGoods;
+    public TableColumn colClimate;
+    public TableColumn colCategory;
+    public TableColumn colisUsed;
+    public TableColumn colAddress;
+
+    public void newSklad(ActionEvent actionEvent) {
+    }
+
+    public void editSklad(ActionEvent actionEvent) {
+    }
+
+    public void removeSklad(ActionEvent actionEvent) {
+    }
+
+    public void editUser(ActionEvent actionEvent) {
+    }
+}
